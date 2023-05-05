9075b5d4dc4294317884bddcbc9f9dea4927ccc5
Edward Karlsson <edwkar@student.chalmers.se>
2023-05-05 10:14:29 +0200

    the pin component now shows the image

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,7 +1,15 @@
-import React from "react"
+import { urlFor } from "../client"
 
-const Pin = () => {
-  return <div>Pin</div>
+const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+  return (
+    <div>
+      <img
+        className="w-full rounded-lg"
+        alt="user-post"
+        src={urlFor(image).width(250).url()}
+      />
+    </div>
+  )
 }
 
 export default Pin
