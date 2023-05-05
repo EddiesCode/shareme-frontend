f788f7fbfb3852f7b0f165ea26df5bb4e1100d1f
Edward Karlsson <edwkar@student.chalmers.se>
2023-05-05 09:21:08 +0200

    designed the Navbar component, it is done

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
 import { Routes, Route } from "react-router-dom"
 
-import { NavBar, Feed, PinDetail, CreatePin, Search } from "../components"
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components"
 
 const Pins = ({ user }) => {
   const [searchTerm, setSearchTerm] = useState("")
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
-        <NavBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Navbar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user}
+        />
       </div>
       <div className="h-full">
         <Routes>
@@ -20,7 +24,7 @@ const Pins = ({ user }) => {
           />
           <Route path="/create-pin" element={<CreatePin user={user} />} />
           <Route
-            path="/searc"
+            path="/search"
             element={
               <Search
                 searchTerm={setSearchTerm}
